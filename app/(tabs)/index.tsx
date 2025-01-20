2c8361754856893063d0b8ca7e417fe8748f37ec
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2025-01-20 23:44:21 +0530

    Add logout functionality to Home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Button from '@/components/Button'
+import Typo from '@/components/Typo'
+import { colors } from '@/constants/theme'
+import { signOut } from 'firebase/auth'
+import { auth } from '@/config/firebase'
 
 const Home = () => {
+    const handleLogOut =async () => {
+
+        // Log out
+        await signOut(auth)
+    }
   return (
     <View>
+        <Text>Home</Text>
+        {/* Home Page Content */}
+        {/* Navigation */}
+        {/* Dynamic Content */}
+        {/* Footer */}
+        {/* Loading Screen */}
+        {/* Error Screen */}
+        {/* Empty Screen */}
+        {/* Profile Screen */}
+        {/* Settings Screen */}
+        <Button onPress={handleLogOut}>
+            <Typo color={colors.black}></Typo>
+        </Button>
     </View>
   )
 }
