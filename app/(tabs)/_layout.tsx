0f6bdec7a92900354cfc44f897dc5e2898ff5a0b
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2025-01-22 11:39:25 +0530

    Add custom tab component and integrate it into the layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import CustonTabs from '@/components/CustonTabs'
 
 const _layout = () => {
   return (
-    <Tabs></Tabs>
+    <Tabs tabBar={CustonTabs}  screenOptions={{headerShown: false}}>
+      {/* Add your screens here */}
+   <Tabs.Screen name="index" options={{title:"index"}} />
+    <Tabs.Screen name="statics" options={{title:"statics"}} />
+    <Tabs.Screen name="wallet" options={{title:"wallet"}} />
+    <Tabs.Screen name="profile" options={{title:"profile"}} />
+
+
+    </Tabs>
   )
 }
 
---
+++ components/CustonTabs.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CustonTabs = () => {
+  return (
+    <View>
+      <Text>CustonTabs</Text>
+    </View>
+  )
+}
+
+export default CustonTabs
+
+const styles = StyleSheet.create({})
