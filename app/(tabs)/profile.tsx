0beb1f2e0812b8cd61b2b44555875bcf6cbfcc50
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2025-01-22 23:26:13 +0530

    Refactor Profile component layout and styles for improved user interface

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,83 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper'
+import { colors, spacingX, spacingY } from '@/constants/theme'
+import { verticalScale } from '@/utils/styling'
 
 const profile = () => {
   return (
     <ScreenWrapper>
-      <Text>profile</Text>
+      <View style={styles.container}>
+        <h/>
+      </View>
     </ScreenWrapper>
   )
 }
 
 export default profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingX._20,
+  },
+  userInfo: {
+    marginTop: verticalScale(30),
+    alignItems: "center",
+    gap: spacingY._15,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    // overflow: "hidden", // Uncomment if needed
+    // position: "absolute",
+  },
+  editIcon: {
+    position: "absolute",
+    bottom: 5,
+    right: 8,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding : 5,
+  },
+ 
+  nameContainer: {
+    alignItems: "center",
+    gap: verticalScale(4),
+  },
+  ListIcon: {
+    height: verticalScale(44),
+    width: verticalScale(44),
+    backgroundColor: colors.neutral500,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: radius._15, // Ensure radius._15 exists in your utilities
+    borderCurve: "continuous", // Optional: check if this property is supported
+  },
+  listItem: {
+    marginBottom: verticalScale(17),
+  },
+  accountOptions: {
+    marginTop: spacingY._35,
+  },
+  flexRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: spacingX._10,
+  },
+});
+
