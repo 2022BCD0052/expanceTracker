bf36d2730ce2f69808f856b9222d1be16568258d
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2025-01-26 23:12:58 +0530

    Update profile component to include radius in theme constants for improved styling

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper'
-import { colors, spacingX, spacingY } from '@/constants/theme'
+import { colors, radius, spacingX, spacingY } from '@/constants/theme'
 import { verticalScale } from '@/utils/styling'
 import Header from '@/components/Header'
 
