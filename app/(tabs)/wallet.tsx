bd7d7b66c627fdfc827bb4e03a0f7f2270279e27
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2025-01-30 23:46:41 +0530

    Update wallet screen background color to black

--- app/(tabs)/wallet.tsx
+++ app/(tabs)/wallet.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper'
+import { colors } from '@/constants/theme'
 
 const wallet = () => {
   return (
-    <ScreenWrapper>
+    <ScreenWrapper style={{backgroundColor:colors.black}}>
       <Text>wallet</Text>
     </ScreenWrapper>
   )
